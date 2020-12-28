2645877a45a9d9081f3014e87ce5a99f45262623
Jacob-K-Valeriano <jacob.k.valeriano@gmail.com>
2020-12-27 21:06:02 -0600

    Add files via upload
    12/27 JS Practice , KhanAcademy while loops intro.

---
+++ 12:27 JS Practice.js
@@ -0,0 +1,88 @@
+"use strict"
+
+// Using while loops
+// Google ClassroomFacebookTwitter
+// Email
+// Problem
+// Note: the println() function prints out a line of text with the value that you pass to it - so if you say println("Hi"), it will output: Hi
+// Consider the following code:
+var i = 0;
+while (i < 3) {
+    println("hi");
+    i++;
+}
+// What does the code output?
+
+correct answer = hi
+				 hi
+				 hi
+
+// Consider the following code:
+var i = 0;
+while (i < 3) {
+    println("hi");
+    i++;
+}
+// println("bye");
+// What does the code output?
+// Choose 1 answer:
+correct answer = hi
+				 hi
+				 hi
+				 bye				 
+
+// Consider the following code:
+var i = 0;
+while (i < 0) {
+  println("hi");
+}
+// What does the code output?
+// Choose 1 answer:
+correct answer = It won't output anything 
+
+// Consider the following code:
+var x = 3;
+var i = 0;
+while (i < 3) {
+  x += 1;
+  i += 1;
+}
+println(x);			
+// What does the code output?
+
+correct answer = 6
+
+// Consider the following code:
+var i = 3;
+while (i < 6) {
+  println(i);
+  i += 1;
+}
+// What does the code output?
+correct answer = 3
+				 4
+				 5
+
+// Consider the following code:
+var i = 0;
+while (i < 3) {
+  println(i);
+  i++;
+}
+// What does the code output?
+correct answer = 0
+				 1
+				 2
+
+				 
+
+
+
+
+
+
+
+
+
+
+
