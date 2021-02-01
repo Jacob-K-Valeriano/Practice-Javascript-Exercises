8f48e038b18b76279483e46d49fdfd23a84f40b5
Jacob-K-Valeriano <jacob.k.valeriano@gmail.com>
2021-01-31 22:55:45 -0600

    Add files via upload

---
+++ JS Practice 1:31.js
@@ -0,0 +1,21 @@
+"use strict"
+JS Practice Exercises
+
+
+
+1.) Write a js function that takes in an a array an an argument and returns a new array contatning
+the first and last elements of the passed array.
+
+
+
+2.)JS function that takes in two #'s and returns a boolean representing whether the first number is evenly divisible by the second number passed into the function . If any argument passed into the function is not a number , the function should return false.
+
+3.)Create a function, returnUserFirstLetters, that takes in array of user objects and returns a string of the first letters in lowercase of all the user firstName property values.
+
+4.)Define a function named isANumber that takes in a value and returns true if the input is numeric or not. Numeric strings are not considered as numbers and should return false.
+
+5.)Define a function named increment that takes in an input and adds 1 to it if the input is numeric. If the input is not numeric, then return false.
+
+6.)Define a function named decrement. If the provided input is numeric, the function should subtract 1 and return the result. If the input is not numeric, decrement should return false.
+
+7.)Write a function named getHighestNumber that takes in 3 arguments. If all 3 inputs are numbers or numeric strings, then return the highest number. If any of the 3 inputs is missing or non-numeric, then return false.
